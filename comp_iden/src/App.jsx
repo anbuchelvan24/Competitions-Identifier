8f558f6390a19e4a2f5fe9d04657072fe23a5835
Harishram18 <99391755+Harishram18@users.noreply.github.com>
2024-04-09 16:16:01 +0530

    user context update

--- comp_iden/src/App.jsx
+++ comp_iden/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import GlobalStyles from "./styles/GlobalStyles";
 import Home from "./pages/Home";
@@ -13,42 +13,43 @@ import Render from "./Render";
 import { useState } from "react";
 
 function App() {
-
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const props = {email: email, password: password, setEmail: setEmail, setPassword: setPassword, setIsAuthenticated: setIsAuthenticated};
+  const props = {
+    email: email,
+    password: password,
+    setEmail: setEmail,
+    setPassword: setPassword,
+    setIsAuthenticated: setIsAuthenticated,
+  };
 
-  
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-        <Route index element={<Navigate replace to="login" />} />
+          <Route index element={<Navigate replace to="login" />} />
           <Route element={<AppLayout />}>
-
             {!isAuthenticated ? (
               <Route index element={<Navigate replace to="login" />} />
-
+            ) : (
               // we can implement an 'unauthorized page' (now it defaults to page not found)
 
-            ): 
               <>
                 <Route path="home" element={<Home />} />
                 <Route path="profile" element={<Profile />} />
                 <Route path="dashboard" element={<Render />} />
                 <Route path="calendar" element={<Calendar />} />
-                <Route path="gallery" element={<Gallery />} /> 
+                <Route path="gallery" element={<Gallery />} />
               </>
-            }
+            )}
             <Route path="home" element={<Home />} />
             <Route path="profile" element={<Profile />} />
             <Route path="dashboard" element={<Render />} />
             <Route path="calendar" element={<Calendar />} />
             <Route path="gallery" element={<Gallery />} />
-
           </Route>
 
           <Route path="login" element={<Login loginProps={props} />} />
